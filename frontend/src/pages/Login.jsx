5ca530fa3d76afb635dfe8dd61471617e501e8e8
JholfrankRC <juandavid.janleo02@gmail.com>
2022-10-29 14:17:09 -0500

    New-css

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -6,38 +6,27 @@ import Button from '@mui/material/Button';
 const Login = () => {
   return (
     
-    <div>
-      
- 
-      <div>Login<div>
 
-      <div>
+    <div className='one'> 
 
-      <TextField id="filled-basic" label="Usuario" variant="filled" />
-      <TextField id="filled-basic" label="Contraseña" variant="filled" />
-      <Button variant="contained" href="#contained-buttons">
-          Link
-        </Button>
-        
-      </div>
+        <label className='title'>Login</label>
+        <div className='btn-text'> 
+          <TextField sx={{margin:3, borderRadius:3}} type={'text'} id="filled-basic" label="Usuario" variant="outlined" />
+          <TextField sx={{margin:3, borderRadius:3}} type={'password'} id="filled-basic" label="Contraseña" variant="outlined" />
+          <Button sx={{margin:6, borderRadius:3}} variant="contained" href="#contained-buttons"> Link </Button>
+            
+              <div>
+                  <NavLink to="/registro">
+                    Registrate
+                  </NavLink>
+              </div>
+              <div> 
+                  <NavLink to="/olvide-clave">
+                    Olvidaste la contraseña
+                  </NavLink>
+              </div>
 
-      </div> 
-
-
-        <div>
-          <NavLink to="/registro">
-            Registrate
-          </NavLink>
-        </div> 
-
-        <div>
-          <NavLink to="/olvide-clave">
-            Olvide Clave
-          </NavLink>
-        </div>      
-
-      </div>
-
+        </div>
     </div>
 
   )
