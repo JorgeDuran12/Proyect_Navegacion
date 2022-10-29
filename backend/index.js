f2a5a07e963a2f3fef819aeb12f66e9b2aa0427a
JholfrankRC <juandavid.janleo02@gmail.com>
2022-10-29 15:38:38 -0500

    backend actualizado

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,23 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors"
+import conectarDB from './config/db.js';
 
+dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
 const app = express();
 
+app.use(express.json());
+
+conectarDB();
 
 //Middleware
 app.use('/', (req, res)=>{
-    res.send("Hola Mundo ExpressJS");
+    res.json("Hola Mundo ExpressJS");
 });
 
 app.listen(PORT, () => {
-    console.log(` Server listening on ${PORT}`);
+    console.log(` Servidor encendido en el puerto # ${PORT}`);
 });
